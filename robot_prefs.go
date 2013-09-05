02a5686d1e7be154004a3450668cd34a81a7b434
futuresense <future_sense@mailinator.com>
2013-09-05 12:23:37 -0700

    nagasaka

--- robot_prefs.go
+++ robot_prefs.go
@@ -17,30 +17,48 @@ type Robot struct {
 	Startup_phrase string
 }
 
+var (
+	quirky = &Robot{Name: "Diggity Dan", Musical_tastes: "Rock, Opera, Blues"}
+)
+
 //Check robotpreferences.json exists
 
-func robotPrefs() {
+func init() {
 
 	filebyte, err := ioutil.ReadFile("robotpreferences.json")
 	if err != nil {
 		fmt.Println("Hold on, need to do some setup to use this Robot.")
-		setRobotPrefs()
+		checkForEditor()
+		decideRobotPersonality()
 	}
 	var r Robot
 	json.Unmarshal(filebyte, &r)
 	fmt.Printf("Name: %s \nMusical Tastes: %s", r.Name, r.Musical_tastes)
 }
 
-func setRobotPrefs() {
+func decideRobotPersonality() {
+	fmt.Println(`"(*&(*&()*&@()&*@()*&@()*&@()&*@(MAGICAWESOMEPOWER))))"`)
+
+}
+
+func checkForPrefsFile() {
+
 	f, _ := os.Create("./robotpreferences.json")
 	defer f.Close()
 	r := Robot{"Bob", "Rock", "Hi there"}
 	b, _ := json.Marshal(r)
+
+}
+func checkForEditor() {
 	subl, err := exec.LookPath("subl")
 	if err != nil {
 		fmt.Println("Please symlink sublime text to command line, \"subl\", then restart this program. %s", subl)
 	}
 
 	fmt.Println(b)
+
+}
+func getLocalSongsAndProcess() {
+	//Todo
 
 }
